7516a0f2bb7146fa7abc29b4f90e3dc3025daee6
Prantho <pranthodey89@gmail.com>
2021-03-01 02:18:34 +0600

    new file added

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,6 +66,26 @@
 		});
 	};
 
+	// Protfolio Slider
+	$('.protfolio-active').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		center:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:3
+			}
+		}
+	});
+
+
 	/*------------------------------------
         Overlay Close
 	--------------------------------------*/
